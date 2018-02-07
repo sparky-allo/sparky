7d0d0452bfae3415df99d20a391d760da92db757
Sudeep <sudeepkumar@cem-solutions.net>
2018-02-07 12:55:58 +0530

    dac id update1

--- sound/usb/quirks.c
+++ sound/usb/quirks.c
@@ -1027,6 +1027,7 @@ u64 snd_usb_interface_dsd_format_quirks(struct snd_usb_audio *chip,
 	case USB_ID(0x20a0, 0x4143): /* WaveIO USB Audio 2.0 */
 	case USB_ID(0x20b1, 0x3073): /* Soekris dac1101 */
 	case USB_ID(0x20b1, 0x300f): /* Engineered Electronics Stereo Playback Interface */
+	case USB_ID(0x20b1, 0x3066): /*Topping D30 (XMOS) */
 		if (fp->altsetting == 3)
 			return SNDRV_PCM_FMTBIT_DSD_U32_BE;
 		break;
